efc7ade98a4c776d8f10d7432cb84752fe28594a
teigue11 <dericknjd@gmail.com>
2026-06-16 21:43:04 -0300

    Create function_jogo.c

---
+++ function_jogo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int validar(char c)
+{
+    return (c == 'X' || c == 'O');
+}
+
+void gravar(char matriz[3][3], char nomeArquivo[])
+{
+    FILE *arquivo = fopen(nomeArquivo, "w");
+
+    if (arquivo == NULL)
+    {
+        printf("Erro ao criar arquivo\n");
+        return;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        fprintf(arquivo, "| %c | %c | %c |\n",
+                matriz[i][0], matriz[i][1], matriz[i][2]);
+    }
+
+    fprintf(arquivo, "\nCriado por Davi Sales");
+
+    fclose(arquivo);
+}
